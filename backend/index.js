9d9b52a0d58880648c53514ca440c4c9611e7351
MetaMyles <mylesbyrne01@gmail.com>
2023-08-18 16:04:26 -0400

    establishing conection

--- backend/index.js
+++ backend/index.js
@@ -7,10 +7,16 @@ const MongoClient = mongodb.MongoClient
 const port = process.env.PORT || 8000
 
 MongoClient.connect(
-    process.env.RESTREVIEWS_DB_URI
-    {
-        poolSize: 50
-        wtimeout: 2500
-        useBewUrlParse: true
+  process.env.RESTREVIEWS_DB_URI,
+  {
     }
-)
+  )
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+  })
