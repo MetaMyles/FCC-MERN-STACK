314e4dcf4bf1a70e6c5995bc7e8fab059df05a8b
MetaMyles <mylesbyrne01@gmail.com>
2023-08-27 10:06:46 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 dotenv.config();
 
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => res.send('API running'));
-
 app.listen(port, () => console.log(`Server started on port ${port}`));
 app.use('/api/users', userRoutes);
+app.use(notFound);
+app.use(errorHandler);
