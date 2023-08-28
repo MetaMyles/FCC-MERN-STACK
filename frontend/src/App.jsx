bdede49be284b6a0c6bac06ffea63edff84ae05f
MetaMyles <mylesbyrne01@gmail.com>
2023-08-28 11:13:21 -0400

    creating header and Hero components + Homescreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
+import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
+import Header from './components/Header';
+
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (<><Header /><Container className='my-2'><HomeScreen /></Container></>);
 };
 
 export default App;
