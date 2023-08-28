43c195b4e5ebfe2dd15a588b786a817c99e14432
MetaMyles <mylesbyrne01@gmail.com>
2023-08-28 09:46:23 -0400

    Cookie Parser Middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+
 
 dotenv.config();
 
@@ -17,6 +19,7 @@ app.listen(port, () => console.log(`Server started on port ${port}`));
 /**/ 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 /**/
 
 app.use('/api/users', userRoutes);
