27d6f11fc42f5082e2611ad479be991a95d6feb7
MetaMyles <mylesbyrne01@gmail.com>
2023-08-28 09:09:55 -0400

    Request Body Parser Middleware

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,19 @@ dotenv.config();
 
 const port = process.env.PORT || 5000;
 connectDB();
-
 const app = express();
+
+
+
 
 app.get('/', (req, res) => res.send('API running'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+/**/ 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+/**/
+
 app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
