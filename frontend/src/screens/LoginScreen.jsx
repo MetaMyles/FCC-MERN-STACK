30c503e3ea76464f829fd97622c38c37e2c9f1b4
MylesByrne <mylesbyrne01@gmail.com>
2023-08-28 14:06:02 -0400

    Added loader component and LoginScreen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -5,6 +5,8 @@ import FormContainer from '../components/FormContainer';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLoginMutation } from '../slices/usersApiSlice';
 import { setCredentials } from '../slices/authSlice';
+import { toast } from 'react-toastify';
+import Loader from '../components/Loader';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
@@ -30,7 +32,7 @@ const LoginScreen = () => {
       dispatch(setCredentials({ ...res }));
       navigate('/');
     } catch (err) {
-      console.log(err?.data?.message || err.error);
+      toast.error(err?.data?.message || err.error);
     }
   };
 
@@ -69,7 +71,7 @@ const LoginScreen = () => {
         </Button>
       </Form>
 
-      {isLoading && <p>Loading...</p>}
+      {isLoading && <Loader />}
 
       <Row className='py-3'>
         <Col>
