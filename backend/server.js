442a7e00e3497359d6dbde6f14f845017e653519
MetaMyles <mylesbyrne01@gmail.com>
2023-08-28 09:14:38 -0400

    hashing Passwords

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,6 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 
-
-
-
 app.get('/', (req, res) => res.send('API running'));
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
