382b0612975413774a735fdbf11111ea90732abc
MetaMyles <mylesbyrne01@gmail.com>
2023-08-28 11:16:02 -0400

    React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { Container } from 'react-bootstrap';
-import HomeScreen from './screens/HomeScreen';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 
-
 const App = () => {
-  return (<><Header /><Container className='my-2'><HomeScreen /></Container></>);
+  return (
+    <>
+      <Header />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
+    </>
+  );
 };
 
 export default App;
