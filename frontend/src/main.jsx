07e9168102ab8b5d747b34d240f82f2a9d32f83a
MetaMyles <mylesbyrne01@gmail.com>
2023-08-28 11:33:33 -0400

    slices

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,17 +10,25 @@ import {
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen.jsx';
+import RegisterScreen from './screens/RegisterScreen.jsx';
+import store from './store';
+import { Provider } from 'react-redux';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen/>}/>
+      <Route path='/register' element={<RegisterScreen/>}/>
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
+  </Provider>
 );
